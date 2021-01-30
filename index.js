542ee79feb4369007bd765f365098b450591f8cf
SpeckyYT <Specky.fans@gmail.com>
2021-01-30 11:35:32 +0100

    implemented waifulabs module

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const fetch = require("node-fetch");
-const api = (path = "", data = {}) => fetch(`https://api.waifulabs.com/${path}`, {
-        method: "POST", body: JSON.stringify(data) }).then(res => res.json());
+const waifulabs = require('waifulabs');
 /** @param {{ seeds: (number | number[])[] }} v */
-const bufferer = v => ({ image: Buffer.from(v && (v.image || v.girl) || "", "base64"), seeds: v && v.seeds });
+const bufferer = v => (
+    {
+        image: Buffer.from(v && v.image || "", "base64"),
+        seeds: v && v.seeds
+    }
+);
 
-module.exports = class Waifu {
+module.exports = class WaifuLabsAPI {
     // Constructor is only called for a newly generated waifu
     constructor(data = []) {
         this.curr = bufferer().image; // 0 byte buffer just to type this
@@ -25,7 +28,7 @@ module.exports = class Waifu {
     }
 
     async refresh() {
-        this.data = (await api("generate", { step: this.step, currentGirl: this.seeds })).newGirls.map(bufferer);
+        this.data = (await waifulabs.generateWaifus(this.seeds, this.step)).map(bufferer);
         return this;
     }
 
@@ -33,16 +36,18 @@ module.exports = class Waifu {
     async proceed(choice = this.step ? -1 : 0) {
         if (this.step >= 3) return;
         this.history.push(Object.assign({}, this));
-        if (this.step && choice >= 0) this.curr = bufferer(await api("generate_big", 
-            { step: this.step, size: 512, currentGirl: this.seeds = this.data[choice].seeds })).image;
+        if (this.step && choice >= 0){
+            this.seeds = this.data[choice].seeds;
+            this.curr = bufferer(await waifulabs.generateBigWaifu(this.seeds)).image;
+        }
         return await this.refresh();
     }
 
     /** @param {"PILLOW"|"POSTER"} product */
     async getProduct(product) {
-        if (!["PILLOW", "POSTER"].includes(product) || !this.step) return null; // Invalid product or first step
-        return bufferer(await api("generate_preview", { currentGirl: this.seeds, product })).image;
+        if(!this.step) return null; // Invalid product or first step
+        return bufferer(await waifulabs.generateProduct(this.seeds, product)).image;
     }
     // Generate a new waifu
-    static generate() { return new Waifu().refresh(); }
+    static generate() { return new WaifuLabsAPI().refresh(); }
 }
